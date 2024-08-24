b63cf74fb3d70aad61c2e30e0ab57d44a137b308
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-08-24 07:32:57 -0300

    Resolução final (vídeo 5.4)

--- src/main/java/br/com/alura/bytebank/PagamentoJobConfiguration.java
+++ src/main/java/br/com/alura/bytebank/PagamentoJobConfiguration.java
@@ -8,10 +8,16 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class PagamentoJobConfiguration {
@@ -34,6 +40,32 @@ public class PagamentoJobConfiguration {
                 .<Pagamento, Pagamento>chunk(10, transactionManager)
                 .reader(leitor)
                 .writer(escrita)
+                .build();
+    }
+
+    @Bean
+    public ItemReader<Pagamento> leitor() {
+        return new FlatFileItemReaderBuilder<Pagamento>()
+                .name("leitor")
+                .resource(new FileSystemResource("files/dados_ficticios.csv"))
+                .linesToSkip(1)
+                .delimited()
+                .delimiter("|")
+                .names("funcionario", "cpf", "agencia", "conta", "valor", "mesReferencia")
+                .targetType(Pagamento.class)
+                .build();
+
+    }
+
+    @Bean
+    public ItemWriter<Pagamento> escrita(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Pagamento>()
+                .dataSource(dataSource)
+                .sql(
+                        "INSERT INTO pagamento (funcionario, cpf, agencia, conta, valor, mes_referencia)" +
+                        " VALUES (:funcionario, :cpf, :agencia, :conta, :valor, :mesReferencia)"
+                )
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
                 .build();
     }
 }
